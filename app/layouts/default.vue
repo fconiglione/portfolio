55e2bdd3fa659d16da65cc6482dfd196ce661f47
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2025-10-20 00:00:18 -0400

    fixed padding issue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen flex flex-col px-4 sm:px-6 md:px-8 lg:px-12 my-4">
+    <div class="min-h-screen flex flex-col px-4 sm:px-8 md:px-16 lg:px-32 xl:px-64 py-4">
         <AppHeader />
         <main class="flex-1">
             <NuxtPage />
