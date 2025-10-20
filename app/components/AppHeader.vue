db598ed48abd60d04604942942ba872318088a66
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2025-10-19 21:34:43 -0400

    installed shadcn

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    Header
-  </div>
+  <header>
+    <div>
+        <span>&lt;/Francesco Coniglione &gt;</span>
+    </div>
+    <div>
+        <nav>
+            <ul>
+                <NuxtLink to="/">Home</NuxtLink>
+                <NuxtLink to="/projects">Projects</NuxtLink>
+                <NuxtLink to="/about">About</NuxtLink>
+                <NuxtLink to="/contact">Contact</NuxtLink>
+            </ul>
+        </nav>
+        <div>
+            <Button>Click me</Button>
+        </div>
+    </div>
+  </header>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt']
+  modules: ['shadcn-nuxt'],
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    prefix: '',
+    componentDir: './app/components/ui',
+  },
 })
